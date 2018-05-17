dc035ab2bcf1c8ba2705a3b01bb674e33499134d
mopecat <liuyangdiv@163.com>
2018-05-17 22:31:14 +0800

    框架基本构成

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        name: "recommend"
+    }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        name: "singer"
+    }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
